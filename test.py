0d0bd541b76c0f3a3ef01ac596096e6e2d054f9c
masaru-tsuruta <mtsuru.rough.cut@gmail.com>
2020-07-30 10:19:34 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,70 @@
+#https://tzmi.hatenablog.com/entry/2020/02/23/232707
+from torch import nn
+
+class cnn(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(6, 16, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(16*5*5, 120),
+            nn.ReLU(inplace=True),
+            nn.Linear(120,84),
+            nn.ReLU(inplace=True),
+            nn.Linear(84, 10),
+            nn.Softmax(dim=1),
+        )
+
+        # weight init                                                                      
+        for m in self.layers.children():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+
+    def forward(self, x):
+        return self.layers(x)
+    
+from torch import nn
+
+class cnn2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.c1 = nn.Conv2d(1, 6, kernel_size=5, padding=2)
+        self.r1 = nn.ReLU(inplace=True)
+        self.m1 = nn.MaxPool2d(2)
+        self.c2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.r2 = nn.ReLU(inplace=True)
+        self.m2 = nn.MaxPool2d(2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.r3 = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(120,84)
+        self.r4 = nn.ReLU(inplace=True)
+        self.fc3 = nn.Linear(84, 10)
+        self.softmax = nn.Softmax(dim=1)
+
+        # weight init                                                                      
+        for m in self.children():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+
+    def forward(self, x):
+        x = self.m1(self.r1(self.c1(x)))
+        x = self.m2(self.r2(self.c2(x)))
+        x = self.flatten(x)
+        x = self.r3(self.fc1(x))
+        x = self.r4(self.fc2(x))
+        x = self.softmax(self.fc3(x))
+        return x
+    
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
